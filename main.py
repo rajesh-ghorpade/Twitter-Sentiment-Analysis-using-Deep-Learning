838d769adfc35fed1a8b82b81d1c2bba2247cd4f
rajesh-ghorpade <rajesh.ghorpade500@gmail.com>
2026-09-03 11:12:32 +0530

    initial commit for RNN Project

---
+++ main.py
@@ -0,0 +1,28 @@
+#Import required libraries
+import streamlit as st
+import tensorflow
+from tensorflow.keras.models import load_model
+import pickle
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+import numpy as np
+
+# Loading tensorflow model for prediction
+model = load_model('model.h5')
+
+with open('tokenizer.pickle','rb') as file:
+    tokenizer = pickle.load(file)
+
+st.title("Twitter Tweets Sentiment Analysis")
+
+tweet = st.text_area("Enter your tweet here:")
+
+if st.button("Predict Sentiment") and tweet.strip():
+    sequences = tokenizer.texts_to_sequences([tweet])
+    sequences = pad_sequences(sequences, maxlen=99, padding='post')
+
+    prediction = model.predict(sequences)
+    predicted_class = np.argmax(prediction, axis=1)[0]
+
+    sentiment_mapping = {0: 'Negative', 1: 'Neutral', 2: 'Positive'}
+
+    st.write(f"Predicted Sentiment: {sentiment_mapping[predicted_class]}")
