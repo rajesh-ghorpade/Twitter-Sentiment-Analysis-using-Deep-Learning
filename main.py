0231f4738657bc6581ba401ec8353343871c70d8
rajesh-ghorpade <66025618+rajesh-ghorpade@users.noreply.github.com>
2026-09-03 12:20:21 +0530

    update tekonizer

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 import streamlit as st
 import tensorflow
 from tensorflow.keras.models import load_model
+from tensorflow.keras.preprocessing.text import Tokenizer
 import pickle
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 import numpy as np
