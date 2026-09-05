4c20cbb55aa81ba6ba32db80b88e031a6b3b5c88
rajesh-ghorpade <66025618+rajesh-ghorpade@users.noreply.github.com>
2026-09-05 09:48:59 +0530

    Refactor string quotes in main.py
    Updated string quotes for consistency and readability.

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-#Import required libraries
 import streamlit as st
 import tensorflow as tf
 from tensorflow.keras.models import load_model
@@ -7,10 +6,9 @@ import pickle
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 import numpy as np
 
-# Loading tensorflow model for prediction
-model = load_model('model.h5')
+model = load_model("model.h5")
 
-with open('tokenizer.pickle','rb') as file:
+with open("tokenizer.pickle", "rb") as file:
     tokenizer = pickle.load(file)
 
 st.title("Twitter Tweets Sentiment Analysis")
@@ -19,11 +17,15 @@ tweet = st.text_area("Enter your tweet here:")
 
 if st.button("Predict Sentiment") and tweet.strip():
     sequences = tokenizer.texts_to_sequences([tweet])
-    sequences = pad_sequences(sequences, maxlen=99, padding='post')
+    sequences = pad_sequences(sequences, maxlen=99, padding="post")
 
     prediction = model.predict(sequences)
     predicted_class = np.argmax(prediction, axis=1)[0]
 
-    sentiment_mapping = {0: 'Negative', 1: 'Neutral', 2: 'Positive'}
+    sentiment_mapping = {
+        0: "Negative",
+        1: "Neutral",
+        2: "Positive"
+    }
 
     st.write(f"Predicted Sentiment: {sentiment_mapping[predicted_class]}")
